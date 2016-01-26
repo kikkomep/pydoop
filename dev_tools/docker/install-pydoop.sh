d36c11f2f8366402ade7f07101201d5d074dba70
Marco Enrico Piras <kikkomep@crs4.it>
2016-01-26 18:02:16 +0100

    Renamed script to install pydoop

--- dev_tools/docker/install-pydoop.sh
+++ dev_tools/docker/install-pydoop.sh
@@ -7,6 +7,15 @@ script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # install pydoop
 cd $script_path
+
+# removing existing versions
+# TODO: update with a more general cleaner
+rm -Rf /root/.local/lib/python2.7/site-packages/pydoop*
+
+# clean install dir
+make clean
+
+# install pydoop
 python setup.py build --hdfs-core-impl $HDFS_BACKEND
 python setup.py install --user --skip-build
 
