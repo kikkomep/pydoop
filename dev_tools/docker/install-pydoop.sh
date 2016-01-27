3d259bf7bf3328faa3a45469f01af84d24766c1e
Marco Enrico Piras <kikkomep@crs4.it>
2016-01-27 10:24:37 +0100

    Added script to install a clean version of pydoop

--- dev_tools/docker/install-pydoop.sh
+++ dev_tools/docker/install-pydoop.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-HDFS_BACKEND=native
+HDFS_BACKEND="native"
 
 # current path
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# install pydoop
-cd $script_path
+# find the pydoop package folder
+dir=$(python - <<EOF
+import os
+import pkgutil
+
+if pkgutil.find_loader('pydoop'):
+    import pydoop
+    print os.path.dirname(pydoop.__file__) + "/../.."
+EOF
+)
 
 # removing existing versions
-# TODO: update with a more general cleaner
-rm -Rf /root/.local/lib/python2.7/site-packages/pydoop*
+if [[ -n ${dir} ]]; then
+    echo "found directory"
+    rm -Rf ${dir}/pydoop*
+else
+    echo "No Pydoop version installed!!!"
+fi
+
+# set the working dir
+working_dir="${script_path}/../../"
+cd ${working_dir}
 
 # clean install dir
 make clean
 
 # install pydoop
-python setup.py build --hdfs-core-impl $HDFS_BACKEND
+python setup.py build --hdfs-core-impl ${HDFS_BACKEND}
 python setup.py install --user --skip-build
-
-# run tests
-cd test && python all_tests.py
